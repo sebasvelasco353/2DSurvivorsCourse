02ade6b9af62a78ec485522fde7fde917b53220e
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2024-07-03 22:04:56 -0500

    Finished video 29: Creating the defeat screen

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,14 +1,12 @@
 extends Node
 
+@onready var player = %Player
 @export var end_screen_scene:PackedScene
-@onready var timer = $Timer
 
 func _ready():
-	timer.timeout.connect(on_timer_timeout)
+	player.health_component.died.connect(on_player_died) 
 
-func get_time_elapsed():
-	return timer.wait_time - timer.time_left
-
-func on_timer_timeout():
+func on_player_died():
 	var end_screen_instance = end_screen_scene.instantiate()
 	add_child(end_screen_instance)
+	end_screen_instance.set_defeat()
--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -2,11 +2,17 @@ extends CanvasLayer
 
 @onready var restart_button = %RestartButton
 @onready var quit_button = %QuitButton
+@onready var title_label = %TitleLabel
+@onready var description_label = %DescriptionLabel
 
 func _ready():
 	get_tree().paused = true
 	restart_button.pressed.connect(on_restart_button_pressed)
 	quit_button.pressed.connect(on_quit_button_pressed)
+
+func set_defeat():
+	title_label.text = "DEFEAT"
+	description_label.text = "You Lost!"
 
 func on_restart_button_pressed():
 	get_tree().paused = false
