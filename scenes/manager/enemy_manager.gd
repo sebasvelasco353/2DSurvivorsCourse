40e095b83f11522db58ff130d96bae2f4f20cb08
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2024-07-04 23:31:41 -0500

    Finished video 30: increase difficulty over time

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -2,14 +2,20 @@ extends Node
 
 const SPAWN_RADIUS = 400
 
+@export var arena_time_manager:Node
 @export var basic_enemy_scene:PackedScene
-@onready var timer = $Timer
 
-# Called when the node enters the scene tree for the first time.
+@onready var timer = $Timer
+var base_spawn_time = 0
+
+
 func _ready():
+	base_spawn_time = timer.wait_time
 	timer.timeout.connect(on_timer_timeout)
+	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
 func on_timer_timeout():
+	timer.start()
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
@@ -21,3 +27,9 @@ func on_timer_timeout():
 	
 	entities_layer.add_child(enemy)
 	enemy.global_position = spawn_position
+
+func on_arena_difficulty_increased(arena_difficulty:int):
+	var time_off = (0.1/12) * arena_difficulty
+	time_off = min(time_off, 0.7)
+	timer.wait_time = base_spawn_time - time_off
+	
