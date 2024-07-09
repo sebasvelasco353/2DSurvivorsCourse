9f14ea251cc2fd8a73e7b0e944c1ecd271a47549
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2024-07-08 21:29:36 -0500

    Finished video 32: Preventing invalid spawn

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -15,19 +15,40 @@ func _ready():
 	timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
+func get_spawn_position() -> Vector2:
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return Vector2.ZERO
+	
+	var result
+	var spawn_position
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	
+	for i in 4:
+		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+		
+		# Raycast Check
+		var query_params = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1)
+		result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_params)
+	
+		if result.is_empty():
+			break
+		else:
+			random_direction = random_direction.rotated(deg_to_rad(90))
+	
+	return spawn_position
+
 func on_timer_timeout():
 	timer.start()
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
-	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
-	var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
 	
 	var enemy = basic_enemy_scene.instantiate() as Node2D
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	
 	entities_layer.add_child(enemy)
-	enemy.global_position = spawn_position
+	enemy.global_position = get_spawn_position()
 
 func on_arena_difficulty_increased(arena_difficulty:int):
 	var time_off = (0.1/12) * arena_difficulty
