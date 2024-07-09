0f9bbe2080201061266373792e834ec89ec683d2
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2024-07-08 20:38:37 -0500

    Finished video 31: Improving TileMap

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -11,6 +11,7 @@ var base_spawn_time = 0
 
 func _ready():
 	base_spawn_time = timer.wait_time
+	print("base_spawn_time", base_spawn_time)
 	timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
@@ -32,4 +33,5 @@ func on_arena_difficulty_increased(arena_difficulty:int):
 	var time_off = (0.1/12) * arena_difficulty
 	time_off = min(time_off, 0.7)
 	timer.wait_time = base_spawn_time - time_off
+	print("timer wait time: ",timer.wait_time)
 	
